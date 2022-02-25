98e722dcd5673002b2f105dbd0e3bf6f4ba4fa5f
HarshSunwani <harshsunwani11@gmail.com>
2022-02-25 16:17:44 +0530

    complete requirements.txt file!

--- Accounts/views.py
+++ Accounts/views.py
@@ -11,7 +11,7 @@ from django.core.mail import EmailMessage, message
 from django.contrib.auth import get_user_model
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text 
+from django.utils.encoding import force_bytes, force_str
 from .utils import generate_token
 from django.http import HttpResponse
 
@@ -41,17 +41,17 @@ def sign_up(request):
             else:
                 user = User.objects.create_user(
                     username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-                user.is_active=False
+                user.is_active = False
                 user.save()
 
                 email_subject = "Account Activation Link"
 
                 email_body = render_to_string('activate.html', {
-                            'user': user,
-                            'domain':get_current_site(request), 
-                            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                            'token': generate_token.make_token(user),
-                        })
+                    'user': user,
+                    'domain': get_current_site(request),
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': generate_token.make_token(user),
+                })
                 email = EmailMessage(
                     email_subject,
                     email_body,
@@ -59,7 +59,8 @@ def sign_up(request):
                     [email],
                 )
                 email.send(fail_silently=False)
-                messages.info(request,'E-mail Verification link has been sent to your e-mail')
+                messages.info(
+                    request, 'E-mail Verification link has been sent to your e-mail')
                 return redirect('/accounts/sign-in')
         else:
             messages.info(request, 'Password not matching')
@@ -71,7 +72,7 @@ def sign_up(request):
 def activate_user(request, uidb64, token):
     User = get_user_model()
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
@@ -83,19 +84,19 @@ def activate_user(request, uidb64, token):
         return HttpResponse('Something went wrong :(')
 
 
-
 def sign_in(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(username= username, password= password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect("/")
         else:
-            messages.info(request, 'Invalid Credentials / E-mail Unverified :(')
+            messages.info(
+                request, 'Invalid Credentials / E-mail Unverified :(')
             return redirect('/accounts/sign-in')
 
     else:
@@ -106,31 +107,33 @@ def logout(request):
     auth.logout(request)
     return redirect('/')
 
+
 @login_required(login_url='/sign-in')
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            
+
             return redirect('/accounts/profile')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-        images = Image.objects.filter(user = request.user)
+        images = Image.objects.filter(user=request.user)
 
-    return render(request, 'profile.html', {'u_form':u_form, 'p_form':p_form, 'images': images})
+    return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form, 'images': images})
 
 
 def viewprofile(request, user):
     images = Image.objects.filter(user=user)
     info = User.objects.filter(id=user)
-    return render(request, 'view profile.html', {'images':images,'info':info})
+    return render(request, 'view profile.html', {'images': images, 'info': info})
 
 
 def about(request):
